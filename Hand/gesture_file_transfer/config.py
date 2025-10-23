675d59be4688e19725967e2f2adbc03a6adc51c1
Abbishek-R <abbishekr24@dsce.ac.in>
2025-10-23 19:10:54 +0530

    Add files via upload

---
+++ Hand/gesture_file_transfer/config.py
@@ -0,0 +1,10 @@
+# config.py
+
+STABLE_THRESHOLD = 15  # number of frames a gesture must remain stable
+
+GESTURE_MAP = {
+    "FIVE": "SELECT",
+    "THUMB_UP": "CONFIRM",
+    "PEACE": "SEND",
+    "FIST": "CANCEL"
+}
